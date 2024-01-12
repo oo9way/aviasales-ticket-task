9246c6c5c4d71911bc5301133664e5e26efcdf5d
MuhammadYusuf <mohammedyousef.dev@gmail.com>
2024-01-13 00:07:45 +0500

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn imagegenerator.wsgi  --bind 0.0.0.0:$PORT
--- app/admin.py
+++ app/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from app.models import ServiceCharge, Flight, FlightPacket
 
 # Register your models here.
+
 admin.site.register([ServiceCharge, Flight, FlightPacket])
